cad15db16ce56df7dc97d3a2ab2e4e3f60c31d12
lestrrat <49281+lestrrat@users.noreply.github.com>
2026-07-03 11:02:12 +0900

    unskip merge-049/051 (merge-source type) (#26)
    * un-skip merge-051 (merge-source type validation)
    
    * un-skip merge-049/051 (merge-source type validation)

--- xslt3/w3c_helpers_test.go
+++ xslt3/w3c_helpers_test.go
@@ -1719,8 +1719,6 @@ var w3cImplicitSkips = map[string]string{
 	// ignore_doc_failure dependency (see tools/xslt3gen getDepsSkipReason),
 	// so it no longer needs a name-based skip here.
 
-	// merge: schema-element instance test on merged items
-	"merge-051":   "schema-element() instance test on merged items fails",
 	"merge-097":   "uri-collection Saxon-format ?select=glob URIs not supported (W3C catalog notes non-interoperable)",
 	"merge-097s":  "uri-collection Saxon-format ?select=glob URIs not supported (W3C catalog notes non-interoperable)",
 	"merge-097sf": "uri-collection Saxon-format ?select=glob URIs not supported (W3C catalog notes non-interoperable)",
